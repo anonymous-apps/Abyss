7fcc78178363ac6e6ed0744e797fa20ee91b48ee
Zackary Frazier <zack11frazier22@gmail.com>
2025-05-11 16:01:11 -0700

    Implement tool usage instructions in conversation prompt

--- packages/intelligence/src/models/prompts/buildConversationPrompt.ts
+++ packages/intelligence/src/models/prompts/buildConversationPrompt.ts
@@ -20,6 +20,7 @@ export async function buildConversationPrompt(thread: ReferencedMessageThreadRec
         currentTurnId = senderId;
     };
 
+    let hasNewToolDefinition = false;
     for (const message of messages) {
         // Start new turn if needed
         if (message.senderId !== currentTurnId) {
@@ -38,6 +39,28 @@ export async function buildConversationPrompt(thread: ReferencedMessageThreadRec
                 prompt.addSubPrompt(addToolDefinitionPrompt.compile(toolDefinitions));
             }
         }
+    }
+
+    const activeToolDefinitions = await thread.getAllActiveToolDefinitions();
+
+    if (!!activeToolDefinitions.length) {
+        prompt.addText(`
+            Here is how you can use the tools you have been given:
+            Respond with XML in the format of 
+            <tool-name-1>
+                <param-name-1>param-value-1</param-name-1>
+                <param-name-2>param-value-2</param-name-2>
+                ...
+            </tool-name-1>
+            <tool-name-2>
+                <param-name-1>param-value-1</param-name-1>
+                <param-name-2>param-value-2</param-name-2>
+                ...
+            </tool-name-2>
+            ...
+            (Note: There may be no parameters for a tool)
+            With that format, we will be able to use the tool in the next turn.
+        `);
     }
 
     startNewTurn('assistant');
